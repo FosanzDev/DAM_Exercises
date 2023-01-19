a8efc62f98d4494a1fb59bc545b65a80a4bb8e6a
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-01-19 12:13:50 +0100

    Ej2 Update

--- T7/T7/src/main/java/com/fosanzdev/tema07/Ej1/Ej1.java
+++ T7/T7/src/main/java/com/fosanzdev/tema07/Ej1/Ej1.java
@@ -4,10 +4,10 @@ public class Ej1 {
 
     public Ej1() {
         Coche[] coches = new Coche[10];
+
         for (int i = 0; i < coches.length; i++) {
             coches[i] = new Coche();
         }
-
 
     }
 }
---
+++ T7/T7/src/main/java/com/fosanzdev/tema07/Ej2/Asignatura.java
@@ -0,0 +1,31 @@
+package com.fosanzdev.tema07.Ej2;
+
+public class Asignatura {
+
+    private String nombre;
+    private int id;
+    private String curso;
+
+    public Asignatura(String nombre, int id, String curso) {
+        this.nombre = nombre;
+        this.id = id;
+        this.curso = curso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "nombre=" + nombre + ", id=" + id + ", curso=" + curso + '}';
+    }
+}
