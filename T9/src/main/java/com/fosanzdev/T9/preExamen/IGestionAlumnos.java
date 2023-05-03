aad165f4da167acdc965ea756c84b992638fc735
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-05-03 12:05:03 +0200

    Added T11

--- T9/src/main/java/com/fosanzdev/T9/preExamen/IGestionAlumnos.java
+++
@@ -1,8 +0,0 @@
-package com.fosanzdev.T9.preExamen;
-
-import com.fosanzdev.T9.Ej5.Alumno;
-
-public interface IGestionAlumnos {
-    void mostrarTodos();
-    Alumno buscarPorNia(int nia);
-}
--- T9/src/main/java/com/fosanzdev/T9/preExamen/test.java
+++ T9/src/main/java/com/fosanzdev/T9/preExamen/test.java
@@ -1,52 +1,12 @@
 package com.fosanzdev.T9.preExamen;
 
 import com.fosanzdev.T9.Ej5.Alumno;
+import com.fosanzdev.jresources.JDeploy;
 
 public class test {
 
-    public class GestorAlumnos implements IGestionAlumnos {
-        private Alumno[] alumnos;
-        private int numAlumnos;
+    public static void main(String[] args) {
+        JDeploy.deployMenu("Prueba preExamen", "-->", false, "1. Alumno", "2. Profesor", "3. Personal de administración", "0. Salir");
 
-        public GestorAlumnos(int maxAlumnos) {
-            alumnos = new Alumno[maxAlumnos];
-            numAlumnos = 0;
-        }
-
-        @Override
-        public void mostrarTodos() {
-            for (int i = 0; i < numAlumnos; i++) {
-                System.out.println(alumnos[i]);
-            }
-        }
-
-        @Override
-        public Alumno buscarPorNia(int nia) {
-            for (int i = 0; i < numAlumnos; i++) {
-                if (alumnos[i].getNia() == nia) {
-                    return alumnos[i];
-                }
-            }
-            return null;
-        }
-
-        public void insertarAlumno(Alumno alumno) {
-            if (numAlumnos < alumnos.length) {
-                alumnos[numAlumnos] = alumno;
-                numAlumnos++;
-            } else {
-                System.out.println("No se pueden insertar más alumnos");
-            }
-        }
-
-        public void eliminarAlumno(int nia) {
-            for (int i = 0; i < numAlumnos; i++) {
-                if (alumnos[i].getNia() == nia) {
-                    alumnos[i] = alumnos[numAlumnos - 1];
-                    alumnos[numAlumnos - 1] = null;
-                    numAlumnos--;
-                }
-            }
-        }
     }
 }
